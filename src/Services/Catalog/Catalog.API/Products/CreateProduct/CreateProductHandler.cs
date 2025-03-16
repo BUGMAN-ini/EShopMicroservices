1eedcbd858bcd07c523564405fa151bbae3b0e59
BUGMAN-ini <bshaverda@gmail.com>
2025-03-16 17:34:47 +0400

    CQRS

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description,string ImageFile, decimal price)
+        : IRequest<CreateProductResponse>;
+    public record CreateProductResponse(Guid id);
+    internal class CreateProductHandler : IRequestHandler<CreateProductCommand, CreateProductResponse>
+    {
+        public Task<CreateProductResponse> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            //business Logic
+            throw new NotImplementedException();
+        }
+    }
+}
