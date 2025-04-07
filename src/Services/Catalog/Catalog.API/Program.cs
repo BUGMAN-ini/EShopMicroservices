d0bfa408cb407d40d1788b8f58ffa1f771daa147
BUGMAN-ini <bshaverda@gmail.com>
2025-04-07 22:01:43 +0400

    added in builder

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCarter();
+builder.Services.AddMarten();
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
