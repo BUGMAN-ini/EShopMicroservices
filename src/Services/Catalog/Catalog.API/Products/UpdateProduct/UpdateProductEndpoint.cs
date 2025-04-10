3f03a36d425670d84fa6906206f75eb15fc3cd0d
BUGMAN-ini <bshaverda@gmail.com>
2025-04-10 21:49:18 +0400

    Added UpdateProductEndpoint

--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductEndpoint.cs
@@ -1,6 +1,31 @@
-﻿namespace Catalog.API.Products.UpdateProduct
+﻿using Catalog.API.Products.GetProducts;
+using System.Diagnostics;
+
+namespace Catalog.API.Products.UpdateProduct
 {
-    public class UpdateProductEndpoint
+    public record UpdateProductRequest(Guid id, string Name, List<string> Category, string Description, string ImageFile, decimal price);
+    public record UpdateProductResponse(bool IsSuccess);
+
+    public class UpdateProductEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPut("/products",
+                async (UpdateProductRequest request, ISender sender) =>
+                {
+                    var command = request.Adapt<UpdateProductCommand>();
+
+                    var result = await sender.Send(command);
+
+                    var response = result.Adapt<UpdateProductResponse>();
+
+                    return Results.Ok(response);
+                })
+                .WithName("Update Products")
+                .Produces<UpdateProductResponse>(StatusCodes.Status201Created)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .WithSummary("Update Product")
+                .WithDescription("Update Product");
+        }
     }
 }
