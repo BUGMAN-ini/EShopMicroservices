3d7b461e0f83638df56a91b79666ebcb9c69e947
BUGMAN-ini <bshaverda@gmail.com>
2025-04-10 21:00:44 +0400

    added GetProductByCategory handler

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,26 @@
+﻿
+using Catalog.API.Models;
+using Catalog.API.Products.GetProductById;
+
+namespace Catalog.API.Products.GetProductByCategory
+{
+    public record GetProductByCategoryQuery(Product Category) : IQuery<GetProductByCategoryResult>;
+    public record GetProductByCategoryResult(Product Product);
+
+    internal class GetProductByCategoryHandler(IDocumentSession session, ILogger<GetProductByCategoryHandler> logger) 
+        : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+    {
+        public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery request, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductsBycategoryQueryHandler.Handle called with {@request}", request);
+            var result = await session.LoadAsync<Product>(request.Category,cancellationToken);
+
+            if (result is null)
+            {
+                throw new ProductNotFoundException();
+            }
+
+            return new GetProductByCategoryResult(result);
+        }
+    }
+}
