53782d8bdac5c22faac7fc02d0e65f06cd3e79be
BUGMAN-ini <bshaverda@gmail.com>
2025-04-11 23:12:15 +0400

    more clear and readable

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -3,14 +3,14 @@ using BuildingBlocks.Behaviors;
 var builder = WebApplication.CreateBuilder(args);
 var assembly = typeof(Program).Assembly;
 
-builder.Services.AddCarter();
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssemblies(assembly);
     config.AddOpenBehavior(typeof(ValidationBehavior<,>));
 });
-
 builder.Services.AddValidatorsFromAssembly(assembly);
+
+builder.Services.AddCarter();
 
 builder.Services.AddMarten(opts =>
 {
