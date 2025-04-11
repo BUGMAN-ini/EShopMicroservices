93a8f7f1804d152afd4e98bb80d0ca130636cc66
BUGMAN-ini <bshaverda@gmail.com>
2025-04-11 22:53:00 +0400

    Added MediatR Pipeline

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,9 +1,24 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿using FluentValidation;
+
+namespace Catalog.API.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, List<string> Category, string Description,string ImageFile, decimal price)
         : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid id);
-    internal class CreateProductHandler(IDocumentSession session) 
+
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name Is Required");
+            RuleFor(x => x.Category).NotEmpty().WithMessage("Category is Required");
+            RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is Required");
+            RuleFor(x => x.price).GreaterThan(0).WithMessage("Price must be great than 0");
+
+        }
+    }
+
+    internal class CreateProductHandler(IDocumentSession session, IValidator<CreateProductCommand> validator) 
         : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
@@ -12,6 +27,13 @@
             //Create Product
             //Save to DataBase
             //return CreateProductResult result
+
+            var result = await validator.ValidateAsync(command, cancellationToken);
+            var msg = result.Errors.Select(x => x.ErrorMessage).ToList();
+            if (msg.Any())
+            {
+                throw new ValidationException(msg.FirstOrDefault());
+            }
 
             var product = new Product
             {
