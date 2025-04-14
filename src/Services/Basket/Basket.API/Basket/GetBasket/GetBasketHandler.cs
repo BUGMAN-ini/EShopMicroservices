45e4aa85140969514c62c9b77703d76a221411b9
BUGMAN-ini <bshaverda@gmail.com>
2025-04-14 20:37:47 +0400

    added validations

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -1,7 +1,18 @@
-﻿namespace Basket.API.Basket.GetBasket
-{
-    public class GetBasketHandler
-    {
+﻿
+using Mapster;
 
+namespace Basket.API.Basket.GetBasket
+{
+    public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+    public record GetBasketResult(ShoppingCart Cart);
+    public class GetBasketHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
+    {
+        public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+        {
+            // TODO: get basket from database
+            //var basket = await _repository.GetBasket(request.Username)
+
+            return new GetBasketResult(new ShoppingCart("swn"));
+        }
     }
 }
--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,6 +1,17 @@
 ﻿namespace Basket.API.Basket.StoreBasket
 {
-    public class StoreBasketHandler
+    public record StoreBasketCommand(ShoppingCart cart) : ICommand<StoreBasketResult>;
+    public record StoreBasketResult(bool IsSuccess);
+
+    public class CommandValidator : AbstractValidator<StoreBasketCommand>
+    {
+        public CommandValidator()
+        {
+            RuleFor(x => x.cart).NotNull().WithMessage("Cart can not be null");
+            RuleFor(x => x.cart.UserName).NotEmpty().WithMessage("USername is required");
+        }
+    }
+    public class StoreBasketHandler : ICommandHandler<>
     {
     }
 }
