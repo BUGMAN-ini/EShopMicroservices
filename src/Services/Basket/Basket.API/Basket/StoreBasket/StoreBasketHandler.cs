6437cb95a4ba1fafb87acb6501d2af2db9201d49
BUGMAN-ini <bshaverda@gmail.com>
2025-04-26 21:29:29 +0400

    gavaswore ragaceebi

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,14 +1,14 @@
 ﻿namespace Basket.API.Basket.StoreBasket
 {
-    public record StoreBasketCommand(ShoppingCart cart) : ICommand<StoreBasketResult>;
+    public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
     public record StoreBasketResult(string userName);
 
     public class CommandValidator : AbstractValidator<StoreBasketCommand>
     {
         public CommandValidator()
         {
-            RuleFor(x => x.cart).NotNull().WithMessage("Cart can not be null");
-            RuleFor(x => x.cart.UserName).NotEmpty().WithMessage("USername is required");
+            RuleFor(x => x.Cart).NotNull().WithMessage("Cart can not be null");
+            RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("USername is required");
         }
     }
     public class StoreBasketHandler(IBasketRepository repo)
@@ -16,7 +16,7 @@
     {
         public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
         {
-            await repo.StoreBasket(command.cart, cancellationToken);
+            await repo.StoreBasket(command.Cart, cancellationToken);
 
             return new StoreBasketResult("swn");
         }
