6642d1e77064ec89649360c8bc24a33abb97a47d
BUGMAN-ini <bshaverda@gmail.com>
2025-05-15 01:20:48 +0400

    adding sqllite and ef tools for migration and sql lite connection string

---
+++ src/Services/Discount/Discount.Grpc/Migrations/20250514211821_AdditionalCreate.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Discount.Grpc.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdditionalCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Coupons",
+                columns: new[] { "Id", "Amount", "Description", "ProductName" },
+                values: new object[,]
+                {
+                    { 1, 150.0, "IPhone Discount", "IPhone X" },
+                    { 2, 100.0, "Samsung Discount", "Samsung S10" },
+                    { 3, 50.0, "Xiaomi Discount", "Xiaomi Mi 10" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Coupons",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Coupons",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Coupons",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
