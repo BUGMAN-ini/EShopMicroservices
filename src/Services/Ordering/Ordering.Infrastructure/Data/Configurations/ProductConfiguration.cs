c92772fa05b05cd3c39022003afce657cfba1df6
BUGMAN-ini <bshaverda@gmail.com>
2025-06-22 23:25:51 +0400

    ModelConfigurations

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/ProductConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Models;
+using Ordering.Domain.ValueObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Data.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Id).HasConversion(
+                productId => productId.Value,
+                DbContextId => ProductId.Of(DbContextId));
+
+            builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
+        }
+    }
+}
