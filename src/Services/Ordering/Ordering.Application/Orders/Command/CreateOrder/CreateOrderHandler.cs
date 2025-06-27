0ecdeed57ec9796ee28b8d3d0fe5ad00b0af535a
BUGMAN-ini <bshaverda@gmail.com>
2025-06-28 00:35:24 +0400

    creating order entity

--- src/Services/Ordering/Ordering.Application/Orders/Command/CreateOrder/CreateOrderHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Command/CreateOrder/CreateOrderHandler.cs
@@ -4,9 +4,25 @@ namespace Ordering.Application.Orders.Command.CreateOrder
 {
     public class CreateOrderHandler : ICommandHandler<CreateOrderCommand, CreateOrderResult>
     {
-        public Task<CreateOrderResult> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+        public async Task<CreateOrderResult> Handle(CreateOrderCommand command, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var order = new Order(
+                OrderId.Of(command.OrderId),
+                command.CustomerId,
+                command.OrderItems.Select(item => new OrderItem(
+                    OrderItemId.Of(item.Id),
+                    item.ProductId,
+                    item.Quantity,
+                    item.Price
+                )).ToList(),
+                Payment.Of(
+                    command.Payment.CardName,
+                    command.Payment.CardNumber,
+                    command.Payment.Expiration,
+                    command.Payment.CVV,
+                    command.Payment.PaymentMethod
+                )
+            );
         }
     }
 }
