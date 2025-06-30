b4d2a074b634959c65d04a78b680b07bbd8e965f
BUGMAN-ini <bshaverda@gmail.com>
2025-06-30 21:42:26 +0400

    customerQuery handlers

---
+++ src/Services/Ordering/Ordering.Application/Orders/Query/GetOrdersByCustomer/GetOrderByCustomerHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Orders.Query.GetOrdersByCustomer
+{
+    public class GetOrderByCustomerHandler(IApplicationDbContext dbcontext) 
+        : IQueryHandler<GetOrderByCustomerQuery, GetOrdersByCustomerResult>
+    {
+        public Task<GetOrdersByCustomerResult> Handle(GetOrderByCustomerQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/Query/GetOrdersByCustomer/GetOrderByCustomerQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Orders.Query.GetOrdersByCustomer
+{
+    public record GetOrderByCustomerQuery(Guid CustomerId) : IQuery<GetOrdersByCustomerResult>;
+
+    public record GetOrdersByCustomerResult(IEnumerable<OrderDto> Orders);
+}
