f108fe05b7dc299385ddafb17c754e7c63becdc8
BUGMAN-ini <bshaverda@gmail.com>
2025-09-11 15:32:52 +0400

    adding orderCreated event

---
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Integration/BasketCheckoutEventHandler.cs
@@ -0,0 +1,34 @@
+﻿using BuildingBlocks.Messaging.Events;
+using MassTransit;
+using Ordering.Application.Orders.Command.CreateOrder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Orders.EventHandlers.Integration
+{
+    public class BasketCheckoutEventHandler
+        (ISender sender, ILogger<BasketCheckoutEventHandler> logger)
+        : IConsumer<BasketCheckoutEvent>
+    {
+        public async Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+        {
+            logger.LogInformation("IntegrationEvent handled: {IntegrationEvent}", context.Message.GetType().Name);
+
+            var command = MapToCreateOrderCommand(context.Message);
+            await sender.Send(command);
+        }
+
+        private CreateOrderCommand MapToCreateOrderCommand(BasketCheckoutEvent message)
+        {
+            var orderId = Guid.NewGuid();
+            var orderDto = new OrderDto(
+                Id: orderId,
+                );
+
+                return new CreateOrderCommand(orderDto);
+        }
+    }
+}
