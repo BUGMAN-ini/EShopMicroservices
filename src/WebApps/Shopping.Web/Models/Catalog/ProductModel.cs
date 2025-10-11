775a49163318874b6df9c5384f4dc2714cc48d14
BUGMAN-ini <bshaverda@gmail.com>
2025-10-11 15:44:08 +0400

    added web app

---
+++ src/WebApps/Shopping.Web/Models/Catalog/ProductModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Shopping.Web.Models.Catalog
+{
+    public class ProductModel
+    {
+        public Guid id { get; set; }
+        public string Name { get; set; } = default!;
+        public List<string> Category { get; set; } = new();
+        public string Description { get; set; } = default!;
+        public string ImageFile { get; set; } = default!;
+        public decimal Price { get; set; }
+
+    }
+
+    public record GetProductsRequest(int? PageNumber = 1, int? PageSize = 10);
+    public record GetProductsResponse(IEnumerable<ProductModel> Products);
+    public record GetProductByIdResponse(ProductModel Product);
+}
