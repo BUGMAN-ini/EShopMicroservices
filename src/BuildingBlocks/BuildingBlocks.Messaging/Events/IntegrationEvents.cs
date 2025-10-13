0c6bffc934418c45a29492f6b3453f5f8bb05403
BUGMAN-ini <bshaverda@gmail.com>
2025-10-13 18:58:17 +0400

    fixing mass transit

--- src/BuildingBlocks/BuildingBlocks.Messaging/Events/IntegrationEvents.cs
+++ src/BuildingBlocks/BuildingBlocks.Messaging/Events/IntegrationEvents.cs
@@ -1,15 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BuildingBlocks.Messaging.Events
+﻿namespace BuildingBlocks.Messaging.Events;
+public record IntegrationEvent
 {
-    public record IntegrationEvent
-    {
-        public Guid Id => Guid.NewGuid();
-        public DateTime OccurredOn => DateTime.Now;
-        public string EventType => GetType().AssemblyQualifiedName;
-    }
+    public Guid Id => Guid.NewGuid();
+    public DateTime OccurredOn => DateTime.Now;
+    public string EventType => GetType().AssemblyQualifiedName;
 }
